dfb76ce1f35a63db2119e19d6319b332a2f63198
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-12 18:21:38 -0400

    added middlewares to the app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ALLOWED_ORIGINS, PORT } from './env';
+import * as helmet from 'helmet';
+import * as compression from 'compression';
+import * as rate_limit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({ origin: ALLOWED_ORIGINS });
+  app.use(helmet());
+  app.use(compression());
+  app.use(
+    rate_limit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100,
+    }),
+  );
+
+  await app.listen(PORT);
 }
+
 bootstrap();
