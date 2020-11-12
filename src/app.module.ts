ce8748c5eb58519ff9cdeb2706d9d03c8d7b21a6
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-12 18:56:49 -0400

    created UsersService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
+import * as Env from './env';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: Env.DB_CONN,
+      entities: [User],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  async findOneByEmail(email: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ email });
+  }
+
+  async findOneByEmailOrFail(email: string): Promise<User> {
+    const user = await this.findOneByEmail(email);
+
+    if (!user) {
+      throw new NotFoundException('The requested user does not exist.');
+    }
+
+    return user;
+  }
+
+  async addUser(user: User) {
+    await this.usersRepository.insert(user);
+  }
+
+  async doesUserExist(email: string): Promise<boolean> {
+    const count = await this.usersRepository.count({ where: { email } });
+
+    return count > 0;
+  }
+}
