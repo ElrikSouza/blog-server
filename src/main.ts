52773bd163b87b9d73b6e1d9e35af6071255e8e8
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-12 18:27:32 -0400

    mend

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  ALLOWED_ORIGINS,
-  PORT,
-  RATE_LIMIT_MAX,
-  RATE_LIMIT_WINDOW,
-} from './env';
+import * as Env from './env';
 import * as helmet from 'helmet';
 import * as compression from 'compression';
 import * as rate_limit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ALLOWED_ORIGINS });
+  app.enableCors({ origin: Env.ALLOWED_ORIGINS });
   app.use(helmet());
   app.use(compression());
   app.use(
     rate_limit({
-      windowMs: parseInt(RATE_LIMIT_WINDOW), // 15 minutes
-      max: parseInt(RATE_LIMIT_MAX),
+      windowMs: parseInt(Env.RATE_LIMIT_WINDOW), // 15 minutes
+      max: parseInt(Env.RATE_LIMIT_MAX),
     }),
   );
 
-  await app.listen(PORT);
+  await app.listen(Env.PORT);
 }
 
 bootstrap();
