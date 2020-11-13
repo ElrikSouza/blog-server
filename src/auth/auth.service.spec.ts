c6e0221511ea05aef1ec4c21492fc14c09d5b134
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-13 10:24:26 -0400

    created AuthService

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,125 @@
+import {
+  ConflictException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { v4 as uuidv4 } from 'uuid';
+import { User } from '../users/user.entity';
+import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
+
+interface Query {
+  where: [{ email: string }, { username: string }];
+}
 
 describe('AuthService', () => {
   let service: AuthService;
+  let jwt: JwtService;
+
+  const _user = {
+    _id: uuidv4(),
+    email: 'example@example.com',
+    username: 'yi',
+    password: '$2b$04$N4GXrOJ18ar.GafMP00si.2xkElwFom6aqjWUp2v0I.ks16QndEVm',
+  };
+
+  const mockRepository = {
+    findOne: jest.fn(async ({ email }: { email: string }) => {
+      if (email == _user.email) {
+        return _user;
+      }
+    }),
+    count: jest.fn(async ({ where: [{ email }, { username }] }: Query) => {
+      if (email === _user.email || username === _user.username) {
+        return 1;
+      }
+      return 0;
+    }),
+    insert: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+      ],
+      imports: [JwtModule.register({ secret: 'test' })],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    jwt = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getUserIfCredentialsMatch', () => {
+    it("should throw an Unauthorized exception if the credentials don't match", async () => {
+      const result = service.getUserIfCredentialsMatch(
+        _user.email,
+        '123456789',
+      );
+
+      expect(result).rejects.toThrow(UnauthorizedException);
+    });
+
+    it("should throw a NotFoundException if the requested user doesn't exist", async () => {
+      const result = service.getUserIfCredentialsMatch(
+        'not_found@example.com',
+        '12345678',
+      );
+
+      expect(result).rejects.toThrow(NotFoundException);
+    });
+
+    it('should return the user data if the credentials match', async () => {
+      const user = await service.getUserIfCredentialsMatch(
+        _user.email,
+        '12345678',
+      );
+
+      expect(user).toEqual(_user);
+    });
+  });
+
+  describe('login', () => {
+    it('should return a valid jwt', async () => {
+      const originalPayload = { email: 'test@example.com', _id: 'test_id' };
+      const { token } = await service.login(originalPayload);
+      const { email, _id } = await jwt.verifyAsync(token);
+
+      expect({ email, _id }).toEqual(originalPayload);
+    });
+  });
+
+  describe('registerUser', () => {
+    it('should throw a ConflictException if the requested email or username are already in use', async () => {
+      const registerPromise = service.registerUser({
+        email: _user.email,
+        username: _user.username,
+        password: '12345678',
+      });
+
+      expect(registerPromise).rejects.toBeInstanceOf(ConflictException);
+    });
+
+    it('should create a new user if the requested email and username are available', async () => {
+      const registerPromise = service.registerUser({
+        email: 'example3@example.com',
+        username: 'aaaaaaaaa',
+        password: '12345678',
+      });
+
+      expect(registerPromise).resolves.not.toThrow();
+    });
   });
 });
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare, hash } from 'bcrypt';
+import { v4 as uuidv4 } from 'uuid';
+import { User } from '../users/user.entity';
+import { UsersService } from '../users/users.service';
+
+interface Token {
+  token: string;
+}
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async getUserIfCredentialsMatch(
+    email: string,
+    password: string,
+  ): Promise<User> {
+    const userData = await this.usersService.findOneByEmailOrFail(email);
+    const doPasswordsMatch = await compare(password, userData.password);
+
+    if (!doPasswordsMatch) {
+      throw new UnauthorizedException('Wrong password.');
+    }
+
+    return userData;
+  }
+
+  async login({ email, _id }: Pick<User, '_id' | 'email'>): Promise<Token> {
+    const token = await this.jwtService.signAsync({ email, _id });
+
+    return { token };
+  }
+
+  async registerUser(user: Omit<User, '_id'>): Promise<void> {
+    const doesUserExist = await this.usersService.doesUserExist(user);
+
+    if (doesUserExist) {
+      throw new ConflictException('This user already exists');
+    }
+
+    const passwordHash = await hash(user.password, 12);
+
+    await this.usersService.addUser({
+      ...user,
+      password: passwordHash,
+      _id: uuidv4(),
+    });
+  }
+}
