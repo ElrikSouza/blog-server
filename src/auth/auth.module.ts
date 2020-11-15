ccbffa34ed7c8544e31bfd5ca15aec79927de0f7
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-14 20:20:41 -0400

    Added missing dependencies of the auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { JWT_SECRET } from '../env';
+import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [UsersModule, JwtModule.register({ secret: JWT_SECRET })],
 })
 export class AuthModule {}
