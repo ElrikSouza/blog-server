d6605dc4c74c15f295abed3164f2bd5436289e8d
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-14 20:21:15 -0400

    created AuthController

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,12 +1,54 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../users/user.entity';
+import { UsersService } from '../users/users.service';
+import { v4 as uuidv4 } from 'uuid';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+interface Query {
+  where: [{ email: string }, { username: string }];
+}
 
 describe('AuthController', () => {
   let controller: AuthController;
 
-  beforeEach(async () => {
+  const _user = {
+    _id: uuidv4(),
+    email: 'example@example.com',
+    username: 'test_user',
+    //12345678
+    password: '$2b$04$4fxMFK92C9dCCuCvQW5U9eHpx2CMq7T7nP/9Jgimn7DDP3NoVD1pC',
+  };
+
+  const mockRepository = {
+    findOne: jest.fn(async ({ email }: { email: string }) => {
+      if (email == _user.email) {
+        return _user;
+      }
+    }),
+    count: jest.fn(async ({ where: [{ email }, { username }] }: Query) => {
+      if (email === _user.email || username === _user.username) {
+        return 1;
+      }
+      return 0;
+    }),
+    insert: jest.fn(),
+  };
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+      ],
+      imports: [JwtModule.register({ secret: 'test' })],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -14,5 +56,28 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should accept valid requests', async () => {
+      const result = await controller.login({
+        email: _user.email,
+        password: '12345678',
+      });
+
+      expect(result).toHaveProperty('token');
+    });
+  });
+
+  describe('signUp', () => {
+    it('should accept valid requests', async () => {
+      const result = await controller.signUp({
+        email: 'test@example.com',
+        username: 'test_username',
+        password: '12345678',
+      });
+
+      expect(result).toEqual({ message: 'User has been created' });
+    });
   });
 });
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './login.dto';
+import { SignUpDto } from './sign-up.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/login')
+  async login(@Body(ValidationPipe) { email, password }: LoginDto) {
+    const user = await this.authService.getUserIfCredentialsMatch(
+      email,
+      password,
+    );
+
+    const token = await this.authService.login(user);
+
+    return token;
+  }
+
+  @Post('/sign_up')
+  async signUp(
+    @Body(new ValidationPipe({ whitelist: true }))
+    signUpDto: SignUpDto,
+  ) {
+    await this.authService.registerUser(signUpDto);
+
+    return { message: 'User has been created' };
+  }
+}
