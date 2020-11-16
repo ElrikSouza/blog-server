f7159b8c63a2ba05f3789cfa8fcdbb4693debd53
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-16 18:41:35 -0400

    created jwt guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,17 @@ import { JWT_SECRET } from '../env';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategey';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [UsersModule, JwtModule.register({ secret: JWT_SECRET })],
+  providers: [AuthService, JwtStrategy],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: JWT_SECRET,
+      signOptions: { expiresIn: '2d' },
+    }),
+  ],
 })
 export class AuthModule {}
