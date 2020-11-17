0a77ff2e7f8228b6da3674eae9c8267505b1d221
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-17 16:50:49 -0400

    created PostsDataService

--- src/posts/posts-data/posts-data.service.ts
+++ src/posts/posts-data/posts-data.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UpdatePostDto } from '../dtos/update-post.dto';
+import { Post } from '../post.entity';
 
 @Injectable()
-export class PostsDataService {}
+export class PostsDataService {
+  constructor(
+    @InjectRepository(Post) private postsRepository: Repository<Post>,
+  ) {}
+
+  async getOwnerId(postId: string): Promise<string | undefined> {
+    const post = await this.postsRepository.findOne(
+      { _id: postId },
+      { select: ['user_id'] },
+    );
+
+    return post?.user_id;
+  }
+
+  async findAll() {
+    return this.postsRepository.find();
+  }
+
+  async findOneById(postId: string): Promise<Post | undefined> {
+    return this.postsRepository.findOne({ _id: postId });
+  }
+
+  async findOneOrFail(postId: string): Promise<Post> {
+    const post = this.findOneById(postId);
+
+    if (!post) {
+      throw new NotFoundException('The requested post does not exist');
+    }
+
+    return post;
+  }
+
+  async add(post: Omit<Post, 'created_at' | 'updated_at'>) {
+    await this.postsRepository.insert(post);
+  }
+
+  async update(post: UpdatePostDto, postId: string) {
+    await this.postsRepository.update({ _id: postId }, post);
+  }
+
+  async remove(postId: string) {
+    await this.postsRepository.delete({ _id: postId });
+  }
+}
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,9 +3,12 @@ import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 import { PostsPermissionsService } from './posts-permissions/posts-permissions.service';
 import { PostsDataService } from './posts-data/posts-data.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './post.entity';
 
 @Module({
   controllers: [PostsController],
   providers: [PostsService, PostsPermissionsService, PostsDataService],
+  imports: [TypeOrmModule.forFeature([Post])],
 })
 export class PostsModule {}
