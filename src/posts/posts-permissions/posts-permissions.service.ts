f8347e850bba93822d5e60082eb87e6bdd766d51
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-17 16:53:10 -0400

    created PostsPermissionsService

--- src/posts/posts-permissions/posts-permissions.service.ts
+++ src/posts/posts-permissions/posts-permissions.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PostIdUserIdPair } from '../post-id-user-id-pair';
+import { PostsDataService } from '../posts-data/posts-data.service';
 
 @Injectable()
-export class PostsPermissionsService {}
+export class PostsPermissionsService {
+  constructor(private postsData: PostsDataService) {}
+
+  async assertUserHasPermission({ userId, postId }: PostIdUserIdPair) {
+    const ownerId = await this.postsData.getOwnerId(postId);
+
+    if (!ownerId) {
+      throw new NotFoundException('The requested post does not exist.');
+    }
+
+    if (ownerId !== userId) {
+      throw new ForbiddenException(
+        'This user does not have permission to modify this post.',
+      );
+    }
+  }
+}
