cb499ae26a924cbf1948ff7d056b922dbea39ffc
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-24 14:34:49 -0400

    remove sample code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
@@ -20,7 +18,5 @@ import { Post } from './posts/post.entity';
     AuthModule,
     PostsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
